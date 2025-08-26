ecdad36e78cf7e5817cc8f6e30c3472582794abe
hidejah <hideki@iwasaki.co>
2025-08-26 15:54:21 +0900

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["127.0.0.1", "hidejah.pythonanywhere.com"]
 
