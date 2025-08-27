e8f6a1024c71cc8cb49021cb03af539d5a0accf6
hidejah <hideki@iwasaki.co>
2025-08-27 09:08:05 +0900

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "hidejah.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "blogapp.pythonanywhere.com"]
 
 
 # Application definition
